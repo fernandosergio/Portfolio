0a0077c9c7fc20d97f2a06ee1d9b181d28986953
fyzn <46656725+fyzn@users.noreply.github.com>
2020-09-27 22:46:32 -0300

    modal

---
+++ public/javascript/sidebar.js
@@ -0,0 +1,12 @@
+const sidebar = document.querySelector('.sidebar');
+const close = document.querySelector('.fecha');
+
+function eitanois() {
+    sidebar.classList.toggle('active');
+    close.classList.toggle('active');
+};
+
+function fecha() {
+    sidebar.classList.toggle('active');
+    close.classList.toggle('active');
+};
