c39afb4fd0d77b29191fc04065ead579664297ea
fyzn <46656725+fyzn@users.noreply.github.com>
2020-09-28 19:00:56 -0300

    ajustes

--- public/javascript/landing-page.js
+++ public/javascript/landing-page.js
@@ -4,102 +4,113 @@ const modal = document.querySelector('.modal-container')
 const imagemGrande = document.querySelector('.imagem-grande')
 
 const imagem = document.querySelectorAll('.imagem')
+const corzinha = document.querySelectorAll('.titulo,.descricao,.tecnologias')
+
 const [loteria, noteSaves, pomodoro, borderRadius] = imagem
 
 let botoes = document.querySelector('.botoes')
 const [irAoSite, svg, irAoRepositorio] = botoes.children
 
 const diminui = document.querySelectorAll('.bi-arrows-angle-contract')
-diminui.forEach((elemento) => {
-    elemento.addEventListener('click', () => {
-        modal.classList.remove('mostrar')
+
+window.onload = () => {
+
+    corzinha.forEach((elemento) => {
+        elemento.style.backgroundColor = 'inherit'
+        elemento.style.border = 'none'
     })
-})
 
-document.getElementById('modal-container').addEventListener('click', (e) => {
-    if (e.target.id == 'modal-container')
-        modal.classList.remove('mostrar')
-})
-
-if (loteria) {
-
-    const urlSite = 'https://www.verificarresultadoloteria.com.br/'
-    const urlRep = 'https://github.com/fernandosergio/Verificador-de-loteria'
-
-    const loteriaImagem = loteria.children[0]
-    const loteriaZoomin = loteria.children[1]
-
-    loteriaZoomin.addEventListener('click', () => {
-        aumenta(loteriaImagem, urlSite, urlRep)
+    diminui.forEach((elemento) => {
+        elemento.addEventListener('click', () => {
+            modal.classList.remove('mostrar')
+        })
     })
-}
 
-if (noteSaves) {
-
-    const urlSite = ''
-    const urlRep = 'https://github.com/fernandosergio/note-saves'
-
-    const noteSavesImagem = noteSaves.children[0]
-    const noteSavesZoomIn = noteSaves.children[1]
-
-    noteSavesZoomIn.addEventListener('click', () => {
-        aumenta(noteSavesImagem, urlSite, urlRep)
+    document.getElementById('modal-container').addEventListener('click', (e) => {
+        if (e.target.id == 'modal-container')
+            modal.classList.remove('mostrar')
     })
-}
 
-if (pomodoro) {
+    if (loteria) {
 
-    const urlSite = 'https://fernandosergio.github.io/Pomodore-Clock/'
-    const urlRep = 'https://github.com/fernandosergio/Pomodore-Clock'
+        const urlSite = 'https://www.verificarresultadoloteria.com.br/'
+        const urlRep = 'https://github.com/fernandosergio/Verificador-de-loteria'
 
-    const pomodoroImagem = pomodoro.children[0]
-    const pomodoZoomIn = pomodoro.children[1]
+        const loteriaImagem = loteria.children[0]
+        const loteriaZoomin = loteria.children[1]
 
-    pomodoZoomIn.addEventListener('click', () => {
-        aumenta(pomodoroImagem, urlSite, urlRep)
-    })
-}
-
-if (borderRadius) {
-
-    const urlSite = 'https://fernandosergio.github.io/Border-radius/'
-    const urlRep = 'https://github.com/fernandosergio/Border-radius'
-
-    const borderRadiusImagem = borderRadius.children[0]
-    const borderRadiusZoomIn = borderRadius.children[1]
-
-    borderRadiusZoomIn.addEventListener('click', () => {
-        aumenta(borderRadiusImagem, urlSite, urlRep)
-    })
-}
-
-function aumenta(imagem, site, repositorio) {
-    const quantos = imagemGrande.children
-
-    if (quantos.length > 0) {
-        quantos[0].remove()
+        loteriaZoomin.addEventListener('click', () => {
+            aumenta(loteriaImagem, urlSite, urlRep)
+        })
     }
 
-    let img = document.createElement('img')
+    if (noteSaves) {
 
-    img.src = imagem.src
-    img.alt = imagem.alt
+        const urlSite = ''
+        const urlRep = 'https://github.com/fernandosergio/note-saves'
 
-    imagemGrande.append(img)
-    modal.classList.add('mostrar')
+        const noteSavesImagem = noteSaves.children[0]
+        const noteSavesZoomIn = noteSaves.children[1]
 
-    if (site.length == 0) {
-        console.log('eta nois')
-        irAoSite.children[0].disabled = true
-    } else {
-        irAoSite.children[0].disabled = false
+        noteSavesZoomIn.addEventListener('click', () => {
+            aumenta(noteSavesImagem, urlSite, urlRep)
+        })
     }
 
-    irAoSite.href = ''
-    irAoRepositorio.href = ''
+    if (pomodoro) {
 
-    irAoSite.href = site
-    irAoRepositorio.href = repositorio
+        const urlSite = 'https://fernandosergio.github.io/Pomodore-Clock/'
+        const urlRep = 'https://github.com/fernandosergio/Pomodore-Clock'
+
+        const pomodoroImagem = pomodoro.children[0]
+        const pomodoZoomIn = pomodoro.children[1]
+
+        pomodoZoomIn.addEventListener('click', () => {
+            aumenta(pomodoroImagem, urlSite, urlRep)
+        })
+    }
+
+    if (borderRadius) {
+
+        const urlSite = 'https://fernandosergio.github.io/Border-radius/'
+        const urlRep = 'https://github.com/fernandosergio/Border-radius'
+
+        const borderRadiusImagem = borderRadius.children[0]
+        const borderRadiusZoomIn = borderRadius.children[1]
+
+        borderRadiusZoomIn.addEventListener('click', () => {
+            aumenta(borderRadiusImagem, urlSite, urlRep)
+        })
+    }
+
+    function aumenta(imagem, site, repositorio) {
+        const quantos = imagemGrande.children
+
+        if (quantos.length > 0) {
+            quantos[0].remove()
+        }
+
+        let img = document.createElement('img')
+
+        img.src = imagem.src
+        img.alt = imagem.alt
+
+        imagemGrande.append(img)
+        modal.classList.add('mostrar')
+
+        if (site.length == 0) {
+            console.log('eta nois')
+            irAoSite.children[0].disabled = true
+        } else {
+            irAoSite.children[0].disabled = false
+        }
+
+        irAoSite.href = ''
+        irAoRepositorio.href = ''
+
+        irAoSite.href = site
+        irAoRepositorio.href = repositorio
 
 
+    }
 }
